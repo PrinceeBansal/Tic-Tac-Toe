7498cd1127579401db0e23ed5c483cd550d45030
PrinceeBansal <79979799+PrinceeBansal@users.noreply.github.com>
2023-07-06 12:28:03 +0530

    Update Board.java

--- Tic Tac Toe/Board.java
+++ Tic Tac Toe/Board.java
@@ -2,4 +2,25 @@ package tic_tac_toe;
 
 public class Board {
 
+  private char board[][];
+	private int boardSize = 3;
+	private char p1symbol;
+	private char p2symbol;
+	private int count;
+	
+	public Board(char p1symbol, char p2symbol) {
+		this.p1symbol = p1symbol;
+		this.p2symbol = p2symbol;
+		
+		board = new char[boardSize][boardSize];
+		
+		for(int i = 0; i < boardSize; i++) {
+			for(int j =0; j < boardSize; j++) {
+				board[i][j] = ' ';
+			}
+		}
+	}
+
+  
+
 }
